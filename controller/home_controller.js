97778f38b2cc270587b5e660ea4403a7620d0be8
AshishLal07 <74709197+AshishLal07@users.noreply.github.com>
2022-12-09 16:27:50 +0530

    added views engine and set view folder provide html folder for url

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,7 +1,12 @@
 // module.exports.actionName = function(req,res){}
 
-module.exports.home = function(req, res){
-    // console.log(res);
-    return res.end('<h1>Express up for Major Project</h1>')
-    
+module.exports.home = function(req,res){
+        return res.render('home',{
+            title:'Home'
+        })
 }
+
+
+
+
+
--- controller/users_controller.js
+++ controller/users_controller.js
@@ -3,5 +3,7 @@ module.exports.profile = function(req, res){
     return res.end('<h1>User profile</h1>')
 }
 module.exports.name = function(req,res){
-    return res.end('<h1>Your name in this page</h1>')
+    return res.render('user',{
+        name:'ashish'
+    })
 }
